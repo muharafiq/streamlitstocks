565d9bc9e69044c1fdea2b35ddfce27f83e1f582
muharafiq <142070684+muharafiq@users.noreply.github.com>
2023-08-14 17:52:40 -0500

    Update stockpredictor.py

--- stockpredictor.py
+++ stockpredictor.py
@@ -6,11 +6,8 @@ from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.stattools import adfuller
 import matplotlib.pyplot as plt
 
-# Set page title
-st.title("Stock Price Prediction")
-
 # Data Collection
-ticker = st.text_input("Enter stock ticker symbol:", value="AAPL")
+ticker = input("Enter stock ticker symbol: ")
 start_date = "2010-01-01"
 end_date = "2023-01-01"
 stock_data = yf.download(ticker, start=start_date, end=end_date)
@@ -20,16 +17,26 @@ stock_data['50MA'] = stock_data['Close'].rolling(window=50).mean()
 stock_data['200MA'] = stock_data['Close'].rolling(window=200).mean()
 
 def calculate_rsi(data, window=14):
-    # ... (keep the same)
+    delta = data['Close'].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
 
-    stock_data['RSI'] = calculate_rsi(stock_data)
-    stock_data['Close_diff'] = stock_data['Close'].diff()
-    stock_data = stock_data.dropna()
+    avg_gain = gain.rolling(window=window).mean()
+    avg_loss = loss.rolling(window=window).mean()
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+calculate_rsi(stock_data)
+
+stock_data['Close_diff'] = stock_data['Close'].diff()
+stock_data = stock_data.dropna()
 
 # Check stationarity
 result = adfuller(stock_data['Close_diff'])
-st.write("ADF Statistic:", result[0])
-st.write("p-value:", result[1])
+print("ADF Statistic:", result[0])
+print("p-value:", result[1])
 
 # Model Selection and Training (ARIMA)
 train_size = int(0.8 * len(stock_data))
@@ -45,16 +52,16 @@ forecast = fitted_model.forecast(steps=forecast_steps)
 forecasted_prices = stock_data['Close'].iloc[train_size - 1] + np.cumsum(forecast)
 
 rmse = np.sqrt(mean_squared_error(stock_data['Close'].iloc[train_size:], forecasted_prices))
-st.write("Root Mean Squared Error:", rmse)
+print("Root Mean Squared Error:", rmse)
 
 # Visualization and Prediction
-st.pyplot(plt.figure(figsize=(12, 6)))
+plt.figure(figsize=(12, 6))
 plt.plot(stock_data.index[train_size:], stock_data['Close'].iloc[train_size:], label="Actual Prices")
 plt.plot(stock_data.index[train_size:], forecasted_prices, color='red', label="Predicted Prices")
 plt.xlabel("Time")
 plt.ylabel("Stock Price")
 plt.title(f"{ticker} Stock Price Prediction")
 plt.legend()
-st.pyplot()
+plt.show()
 
-st.write("Predicted Price for the next day:", forecasted_prices.iloc[-1])
+print("Predicted Price for the next day:", forecasted_prices.iloc[-1])
