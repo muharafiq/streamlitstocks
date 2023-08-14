ce1f8582f28eec3f1ff0be4f512c40138e1221d8
muharafiq <142070684+muharafiq@users.noreply.github.com>
2023-08-13 19:32:00 -0500

    Update stockpredictor.py

--- stockpredictor.py
+++ stockpredictor.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import yfinance as yf
 import numpy as np
 import pandas as pd
@@ -6,8 +7,11 @@ from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.stattools import adfuller
 import matplotlib.pyplot as plt
 
+# Set page title
+st.title("Stock Price Prediction")
+
 # Data Collection
-ticker = input("Enter stock ticker symbol: ")
+ticker = st.text_input("Enter stock ticker symbol:", value="AAPL")
 start_date = "2010-01-01"
 end_date = "2023-01-01"
 stock_data = yf.download(ticker, start=start_date, end=end_date)
@@ -17,16 +21,7 @@ stock_data['50MA'] = stock_data['Close'].rolling(window=50).mean()
 stock_data['200MA'] = stock_data['Close'].rolling(window=200).mean()
 
 def calculate_rsi(data, window=14):
-    delta = data['Close'].diff()
-    gain = delta.where(delta > 0, 0)
-    loss = -delta.where(delta < 0, 0)
-
-    avg_gain = gain.rolling(window=window).mean()
-    avg_loss = loss.rolling(window=window).mean()
-
-    rs = avg_gain / avg_loss
-    rsi = 100 - (100 / (1 + rs))
-    return rsi
+    # ... (keep the same)
 
 stock_data['RSI'] = calculate_rsi(stock_data)
 
@@ -35,8 +30,8 @@ stock_data = stock_data.dropna()
 
 # Check stationarity
 result = adfuller(stock_data['Close_diff'])
-print("ADF Statistic:", result[0])
-print("p-value:", result[1])
+st.write("ADF Statistic:", result[0])
+st.write("p-value:", result[1])
 
 # Model Selection and Training (ARIMA)
 train_size = int(0.8 * len(stock_data))
@@ -52,15 +47,16 @@ forecast = fitted_model.forecast(steps=forecast_steps)
 forecasted_prices = stock_data['Close'].iloc[train_size - 1] + np.cumsum(forecast)
 
 rmse = np.sqrt(mean_squared_error(stock_data['Close'].iloc[train_size:], forecasted_prices))
-print("Root Mean Squared Error:", rmse)
+st.write("Root Mean Squared Error:", rmse)
 
 # Visualization and Prediction
-plt.figure(figsize=(12, 6))
+st.pyplot(plt.figure(figsize=(12, 6)))
 plt.plot(stock_data.index[train_size:], stock_data['Close'].iloc[train_size:], label="Actual Prices")
 plt.plot(stock_data.index[train_size:], forecasted_prices, color='red', label="Predicted Prices")
 plt.xlabel("Time")
 plt.ylabel("Stock Price")
 plt.title(f"{ticker} Stock Price Prediction")
 plt.legend()
-plt.show()
-print("Predicted Price for the next day:", forecasted_prices.iloc[-1])
+st.pyplot()
+
+st.write("Predicted Price for the next day:", forecasted_prices.iloc[-1])
